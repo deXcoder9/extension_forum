b5cb93985cdf424802b2116244e230e1ed340fc0
deXcoder9 <dhrubothejuniorprogrammer@gmail.com>
2024-03-04 02:22:03 +0600

    last

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const cardContaier = document.getElementById("cardContainer");
 let titleCountNumber = 0;
 const fetchData = async () => {
   let res = await fetch(
@@ -6,12 +7,12 @@ const fetchData = async () => {
   let postData = await res.json();
   //   console.log(postData);
   let data = postData.posts;
-
   createPost(data);
+  //   searchThrough(data);
 };
 
 function createPost(postInfo) {
-  const cardContaier = document.getElementById("cardContainer");
+  //   console.log(postInfo);
   const titleAdded = document.getElementById("cardSelections");
   const titleCount = document.getElementById("title-count");
   //   console.log(cardContaier);
@@ -123,7 +124,7 @@ const loadData = async () => {
     "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
   );
   const data = await res.json();
-  console.log(data);
+  //   console.log(data);
   displyingLatestNews(data);
 };
 
@@ -137,7 +138,11 @@ const displyingLatestNews = (data) => {
           <div
             class="w-[373px] rounded-xl h-[470px] px-3 lg:px-7 py-5 border-[1px] border-gray-700 flex flex-col space-y-3">
             <img class="rounded-xl" src="${news.cover_image}" alt="">
-            <p>29 January 2024</p>
+            <p>${
+              news.author.posted_date
+                ? news.author.posted_date
+                : "No published date"
+            }</p>
             <h3 class="font-bold text-[18px]">${news.title}</h3>
             <p class="text-gray-500">${news.description} </p>
             <div class="flex gap-4 items-center ">
@@ -145,13 +150,170 @@ const displyingLatestNews = (data) => {
                 alt="">
               <div>
                 <h4>Cameron Williamson</h4>
-                <p>unknown</p>
+                <p>${
+                  news.author.designation ? news.author.designation : "Unknown"
+                }</p>
               </div>
             </div>
           </div>
         </div>
     `;
     latestNEwsContaienr.appendChild(newDiv);
+  }
+};
+
+// function searchThrough(ass) {
+//   const inputField = document.getElementById("inputField");
+//   const inputFields = inputField.value.toLowerCase();
+//   console.log(inputFields);
+//   for (lil of ass) {
+//     console.log(lil);
+//     const categories = lil.category.toLowerCase();
+//     console.log(categories);
+//     console.log(categories);
+//     if (inputFields === categories) {
+//       //   console.log("matched");
+//     } else {
+//       //   console.log("error");
+//     }
+//   }
+// }
+
+async function searchThrough() {
+  toggleLoading(true);
+
+  const inputField = document.getElementById("inputField");
+  const inputFields = inputField.value.toLowerCase();
+  const titleAdded = document.getElementById("cardSelections");
+  const titleCount = document.getElementById("title-count");
+
+  //   console.log(inputFields);
+  let res = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${inputFields}`
+  );
+  let postData = await res.json();
+  //   console.log(postData.posts);
+  let data = postData.posts;
+  //   console.log(data);
+
+  cardContaier.innerHTML = "";
+  for (let newnews of data) {
+    // console.log(newnews);
+    const categories = newnews.category.toLowerCase();
+    const postItem = document.createElement("div");
+
+    if (inputFields === categories) {
+      if (newnews.isActive) {
+        postItem.innerHTML = `
+          <div id="${newnews.id}" class="flex lg:flex-row flex-col gap-x-5 mb-4 card-bg p-10">
+          <div class="relative flex justify-start "><img class="lg:h-[72px] h-[100px]  rounded-lg"
+          src="${newnews.image}" alt="">
+          <p class="h-3 w-3  bg-[#10B981] rounded-full absolute -top-1 -right-1"></p>
+          </div>
+          <div class=" mx-auto flex flex-col justify-start">
+          <div class="flex gap-4">
+          <p class="card-topic">#${newnews.category}</p>
+          <p class="card-topic">Author: ${newnews.author.name}</p>
+          </div>
+          <div class="lg:py-3">
+          <h4 class="card-head">${newnews.title}</h4>
+          </div>
+          <div class="lg:pb-5">
+          <p class="card-description lg:w-[560px] w-auto ">${newnews.description}</p>
+          </div>
+          <hr>
+          <div class="flex justify-between my-4">
+          <div class="flex gap-7">
+          <p><i class="fa-regular fa-message" style="color: #b0b2b5;"></i> ${newnews.comment_count}</p>
+                  <p><i class="fa-regular fa-eye" style="color: #a5a9b1;"></i> ${newnews.view_count}</p>
+                  <p><i class="fa-regular fa-clock" style="color: #a7acb4;"></i> ${newnews.posted_time}</p>
+                  </div>
+                  <div class="w-10 bg-[#10B981] flex justify-center items-center rounded-3xl">
+                  <img class="h-6  " src="mail.png" alt="mail">
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  `;
+      } else {
+        postItem.innerHTML = `
+                    <div id="${newnews.id}" class="flex lg:flex-row flex-col gap-x-5 mb-4 card-bg p-10">
+                    <div class="relative flex justify-start "><img class="lg:h-[72px] h-[100px]  rounded-lg"
+                    src="${newnews.image}" alt="">
+                    <p class="h-3 w-3 bg-red-600 rounded-full absolute -top-1 -right-1"></p>
+                    </div>
+                    <div class=" mx-auto flex flex-col justify-start">
+                    <div class="flex gap-4">
+                    <p class="card-topic">#${newnews.category}</p>
+                    <p class="card-topic">Author: ${newnews.author.name}</p>
+                    </div>
+                    <div class="lg:py-3">
+                    <h4 class="card-head">${newnews.title}</h4>
+                    </div>
+                    <div class="lg:pb-5">
+                    <p class="card-description lg:w-[560px] w-auto ">${newnews.description}</p>
+                    </div>
+                    <hr>
+                    <div class="flex justify-between my-4">
+                    <div class="flex gap-7">
+                    <p><i class="fa-regular fa-message" style="color: #b0b2b5;"></i> ${newnews.comment_count}</p>
+                    <p><i class="fa-regular fa-eye" style="color: #a5a9b1;"></i> ${newnews.view_count}</p>
+                    <p><i class="fa-regular fa-clock" style="color: #a7acb4;"></i> ${newnews.posted_time}</p>
+                    </div>
+                    <div class="w-10 bg-[#10B981] flex justify-center items-center rounded-3xl">
+                    <img class="h-6  " src="mail.png" alt="mail">
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    `;
+      }
+      cardContaier.appendChild(postItem);
+      const card = document.getElementById(newnews.id);
+
+      card.addEventListener("click", () => {
+        titleCountNumber++;
+        const cards = cardContaier.querySelectorAll(".flex");
+        cards.forEach((card) => {
+          card.classList.remove("card-border");
+        });
+
+        const titleCard = document.createElement("div");
+        titleCard.classList.add(
+          "flex",
+          "gap-5",
+          "justify-between",
+          "items-center",
+          "px-3",
+          "bg-white",
+          "py-4",
+          "rounded-md",
+          "mb-3"
+        );
+        titleCard.innerHTML = `<h3 class="w-[212px] text-[16px] font-bold ">${newnews.title}</h3>
+       <p><i class="fa-regular fa-eye" style="color: #a5a9b1;"></i> <span>${newnews.view_count}</span></p>`;
+        titleAdded.appendChild(titleCard);
+        titleCount.innerText = titleCountNumber;
+        card.classList.add("card-border");
+      });
+    } else {
+      cardContaier.innerHTML =
+        "<p class='text-red-700 font-bold'>No category found!</p>";
+    }
+  }
+  toggleLoading(false);
+}
+
+const toggleLoading = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+
+    setTimeout(function () {
+      loadingSpinner.classList.add("hidden");
+    }, 2000);
+  } else {
+    loadingSpinner.classList.add("hidden");
   }
 };
 
